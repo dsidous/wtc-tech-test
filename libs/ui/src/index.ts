51c0a86d0ad17fb4de8cf9cf3c948cfcf8f45030
Tamas <jonastamas2@gmail.com>
2020-07-08 12:18:26 +0100

    refactor: UI library exports

---
+++ libs/ui/src/index.ts
@@ -0,0 +1,12 @@
+import { QueryProvider } from './lib/hooks/useQuery';
+
+export * from './lib/progress/progress';
+export * from './lib/button/button';
+export * from './lib/buttonLink/buttonLink';
+export * from './lib/icons/icons';
+export * from './lib/grid/grid';
+export * from './lib/detailsBox';
+export * from './lib/movieCard';
+export * from './lib/stars';
+export * from './lib/facets';
+export { QueryProvider, useQuery } from './lib/hooks/useQuery';
